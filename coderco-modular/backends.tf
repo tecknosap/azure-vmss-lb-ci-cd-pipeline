24c7e7077ae24211d434046e16df7593882dc7a7
tecknosap <tecknosap@gmail.com>
2025-03-03 10:55:31 +0000

    initial commit

--- coderco-modular/backends.tf
+++ coderco-modular/backends.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "az-01-rg"
-    storage_account_name  = "stro90123wss"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
+    resource_group_name   = "your rg"
+    storage_account_name  = "your storage account"
+    container_name        = "your container"
+    key                   = "your tf state file"
   }
 }
 
